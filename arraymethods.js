9f08218ae92d2f37171e2029bb1f64e1d02c5cd3
MbusyaNgesa <theengesa@gmail.com>
2024-06-17 20:43:58 +0300

    toString() Method

---
+++ arraymethods.js
@@ -0,0 +1,9 @@
+//toString()
+//Places commas between elements declared in an array
+
+let names = ["Ian", "Allison", "Peter", "Mercy"];
+
+let stringname = names.toString();
+console.log(stringname);
+
+//
