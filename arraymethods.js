af9ed0b9e6258a35d14eeb580248b106a9033154
MbusyaNgesa <theengesa@gmail.com>
2024-06-17 20:48:54 +0300

    join() Method

--- arraymethods.js
+++ arraymethods.js
@@ -6,4 +6,8 @@ let names = ["Ian", "Allison", "Peter", "Mercy"];
 let stringname = names.toString();
 console.log(stringname);
 
-//
+//Does same thing as string, only that element separators can be chosen uniquely(specified)
+let jina = ["Vanessa", "Lara", "Sean", "Baggs"];
+
+let stringName = jina.join(" and ");
+console.log(stringName);
