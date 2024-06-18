c2570f0a4e6e3e00b42350652062f10b4abeb0b9
MbusyaNgesa <theengesa@gmail.com>
2024-06-18 13:08:19 +0300

    splice() method

--- arraymethods.js
+++ arraymethods.js
@@ -20,3 +20,24 @@ let names3 = ["Sarah", "Vero"];
 let join = names.concat(names2, names3);
 
 console.log(join);
+
+//splice()
+//Removes and adds elements to an array
+let names4 = ["Tia", "Tamera", "Rose", "Sina"];
+
+names4.splice(1, 1);
+console.log(names4);
+
+//The above array is mutated and to prevent mtation we add another variable and spread it
+let names5 = ["Tia", "Tamera", "Rose", "Sina"];
+let names5copy = [...names5];
+
+names5copy.splice(2, 1);
+console.log(names5);
+console.log(names5copy);
+
+//Adding using splice
+let names6 = ["Radul", "Emet", "Palmer", "Rose"];
+
+names6.splice(1, 2, "Bill", "Alsina");
+console.log(names6);
